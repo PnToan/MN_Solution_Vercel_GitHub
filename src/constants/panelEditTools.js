26f2ee35688845aeb0cfed8c3e2c99f7793185d0
Nguyên Toản Phạm <ntoan.pham@gmail.com>
2026-06-04 16:36:55 +0700

    Refactor_edit_03

--- src/constants/panelEditTools.js
+++
@@ -1,9 +0,0 @@
-export const PANEL_EDIT_TOOLS = [
-  { id: 'editPanelSelect', label: 'Select', icon: '/icons/toolbar/select.svg' },
-  { id: 'editPanelMove', label: 'Move', icon: '/icons/toolbar/move.svg' },
-  { id: 'editPanelLine', label: 'Line', icon: '/icons/toolbar/line.svg' },
-  { id: 'editPanelRect', label: 'Vẽ hình chữ nhật', icon: '/icons/toolbar/rect.svg' },
-  { id: 'editPanelArc', label: 'Arc', icon: '/icons/toolbar/arc.svg' },
-  { id: 'editPanelCircle', label: 'Vẽ hình tròn', icon: '/icons/toolbar/circle.svg' },
-  { id: 'editPanelTape', label: 'Thước', icon: '/icons/toolbar/tape.svg' }
-]
